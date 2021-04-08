88b944e4ef43b9a6c1836ee871e653748e510314
Santhosh Pulliman <santhosh.pulliman@peacker.com>
2021-04-08 22:04:23 +0530

    Update tariff plans

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "99ead0a37490d847e5f5880d29acc330",
-"index.html": "0c293e45f968c22245e3b84ed8bf9c6f",
-"/": "0c293e45f968c22245e3b84ed8bf9c6f",
-"main.dart.js": "68545c0ee883fb58fccd7e5bf90291bb",
+"index.html": "8392434ecea9dcfe6c59e572121879aa",
+"/": "8392434ecea9dcfe6c59e572121879aa",
+"main.dart.js": "6dcf0ffb3f5c45fb1c0b3247a0bd4abd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "3405a2335b70a75622279bbdc13a04e1",
@@ -16,7 +16,7 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/assets/images/peacker.jpg": "6b544dd96ffeb9ab6356e6011ea21894",
-"assets/assets/json/tariff.json": "6d52e6e1e5998c25f388b42a1ac68477"
+"assets/assets/json/tariff.json": "75227a860be475e5d4d9da2348d49413"
 };
 
 // The application shell files that are downloaded before a service worker can
