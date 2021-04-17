6f709c8ee63e70d374dc4d80060b4cd26be2a934
Santhosh Pulliman <santhosh.pulliman@peacker.com>
2021-04-17 23:36:22 +0530

    Updated driver tariff

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,19 +4,19 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "99ead0a37490d847e5f5880d29acc330",
-"index.html": "8392434ecea9dcfe6c59e572121879aa",
-"/": "8392434ecea9dcfe6c59e572121879aa",
-"main.dart.js": "6dcf0ffb3f5c45fb1c0b3247a0bd4abd",
+"index.html": "60625661219b146ab00b7776321cfa85",
+"/": "60625661219b146ab00b7776321cfa85",
+"main.dart.js": "9d7c6554bacbe7e8167dec7441a586bb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "3405a2335b70a75622279bbdc13a04e1",
 "assets/AssetManifest.json": "00fc8fd2fd07707603116df9c153a9d1",
-"assets/NOTICES": "0843b56b099a28d9cbccfe365ccd5ac8",
+"assets/NOTICES": "bf8ae66f4d4702e79be78aa8e2368c79",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/assets/images/peacker.jpg": "6b544dd96ffeb9ab6356e6011ea21894",
-"assets/assets/json/tariff.json": "75227a860be475e5d4d9da2348d49413"
+"assets/assets/json/tariff.json": "0d984684130059e22ce70815367d7ee5"
 };
 
 // The application shell files that are downloaded before a service worker can
